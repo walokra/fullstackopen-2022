d18663eabbe6eb36964969548fd2ec33f5876fd6
Marko Wallin <marko.wallin@iki.fi>
2022-09-01 09:14:19 +0300

    feat: 4a, 4.3, 4.4.

---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,16 @@
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (array) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes;
+  };
+
+  return array.reduce(reducer, 0);
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+};
