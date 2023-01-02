1cfee78d217b121d005acdd6d5b31530d30d223b
Marko Wallin <marko.wallin@iki.fi>
2023-01-02 11:47:12 +0200

    feat: 4b, 4.12

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -13,6 +13,10 @@ blogsRouter.post("/", async (request, response) => {
     blog.likes = 0;
   }
 
+  if (blog.title === undefined || blog.url === undefined) {
+    return response.status(400).end();
+  }
+
   const result = await blog.save();
   response.status(201).json(result);
 });
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -35,78 +35,90 @@ describe("blogs", () => {
     mongoose.connection.close();
   });
 
-  test("blogs are returned as json", async () => {
-    await api
-      .get("/api/blogs")
-      .expect(200)
-      .expect("Content-Type", /application\/json/);
-  });
+  describe("GET", () => {
+    test("blogs are returned as json", async () => {
+      await api
+        .get("/api/blogs")
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+    });
 
-  test("there are two blogs", async () => {
-    const response = await api.get("/api/blogs");
+    test("there are two blogs", async () => {
+      const response = await api.get("/api/blogs");
 
-    expect(response.body).toHaveLength(2);
-  });
+      expect(response.body).toHaveLength(2);
+    });
 
-  test("all blogs are returned", async () => {
-    const response = await api.get("/api/blogs");
+    test("all blogs are returned", async () => {
+      const response = await api.get("/api/blogs");
 
-    expect(response.body).toHaveLength(initialBlogs.length);
-  });
+      expect(response.body).toHaveLength(initialBlogs.length);
+    });
 
-  test("a specific blog is within the returned blogs", async () => {
-    const response = await api.get("/api/blogs");
+    test("a specific blog is within the returned blogs", async () => {
+      const response = await api.get("/api/blogs");
 
-    const contents = response.body.map((r) => r.title);
+      const contents = response.body.map((r) => r.title);
 
-    expect(contents).toContain("React patterns");
-  });
+      expect(contents).toContain("React patterns");
+    });
 
-  test("blog is identified by id field", async () => {
-    const response = await api.get("/api/blogs");
+    test("blog is identified by id field", async () => {
+      const response = await api.get("/api/blogs");
 
-    response.body.forEach((blog) => {
-      expect(blog.id).toBeDefined();
+      response.body.forEach((blog) => {
+        expect(blog.id).toBeDefined();
+      });
     });
   });
 
-  test("blog can be inserted", async () => {
-    const newBlog = {
-      title: "Way of the kings",
-      author: "Brandon Sanderson",
-      url: "https://google.com",
-      likes: 17,
-    };
+  describe("POST", () => {
+    test("blog can be inserted", async () => {
+      const newBlog = {
+        title: "Way of the kings",
+        author: "Brandon Sanderson",
+        url: "https://google.com",
+        likes: 17,
+      };
 
-    await api
-      .post("/api/blogs")
-      .send(newBlog)
-      .expect(201)
-      .expect("Content-Type", /application\/json/);
+      await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
 
-    const blogs = await helper.getBlogs();
+      const blogs = await helper.getBlogs();
 
-    expect(blogs).toHaveLength(initialBlogs.length + 1);
+      expect(blogs).toHaveLength(initialBlogs.length + 1);
 
-    const contents = blogs.map((n) => n.title);
+      const contents = blogs.map((n) => n.title);
 
-    expect(contents).toContain("Way of the kings");
-  });
+      expect(contents).toContain("Way of the kings");
+    });
 
-  test("likes is defaulted to 0", async () => {
-    const newBlog = {
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    };
+    test("likes is defaulted to 0", async () => {
+      const newBlog = {
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      };
 
-    const blog = await api
-      .post("/api/blogs")
-      .send(newBlog)
-      .expect(201)
-      .expect((res) => {
-        expect(res.body.likes).toBe(0);
-      })
-      .expect("Content-Type", /application\/json/);
+      await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect((res) => {
+          expect(res.body.likes).toBe(0);
+        })
+        .expect("Content-Type", /application\/json/);
+    });
+
+    test("fails with status code 400 if title and url is missing", async () => {
+      const invalidBlog = {
+        author: "Robert C. Martin",
+      };
+
+      await api.post("/api/blogs").send(invalidBlog).expect(400);
+    });
   });
 });
