7c257b4eddc04b7cbeb3e950b7aa9e480e46bdc7
Marko Wallin <marko.wallin@iki.fi>
2023-03-13 08:34:49 +0200

    feat: 5b, 5.5, 5.6

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,18 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Blog from "./components/Blog";
 import Notification from "./components/Notification";
 import { create, getAll } from "./services/blogs";
 import LoginForm from "./components/LoginForm";
+import BlogForm from "./components/BlogForm";
+import Togglable from "./components/Togglable";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [message, setMessage] = useState(null);
   const [user, setUser] = useState(null);
-  const [state, setState] = useState({
-    title: "",
-    author: "",
-    url: "",
-  });
+  const blogFormRef = useRef()
+
   const [notificationType, setNotificationType] = useState("success");
 
   useEffect(() => {
@@ -27,6 +26,27 @@ const App = () => {
     setTimeout(() => {
       setMessage(null);
     }, 3000);
+  };
+
+  const addBlog = async (blogObject) => {
+    blogFormRef.current.toggleVisibility()
+
+    try {
+      const addedBlog = await create(blogObject);
+      showMessage(
+        `a new blog '${addedBlog.title}' by ${addedBlog.author} added`,
+        "success"
+      );
+    } catch (error) {
+      console.log(error);
+      showMessage(
+        `an error happened: ${JSON.stringify(error.response.statusText)}`,
+        "error"
+      );
+    }
+
+    const blogs = await getAll();
+    setBlogs(blogs);
   };
 
   const handleError = (message, type = "error") => {
@@ -50,65 +70,10 @@ const App = () => {
     </form>
   );
 
-  const addBlog = async (event) => {
-    event.preventDefault();
-    try {
-      const addedBlog = await create(state);
-      showMessage(`a new blog '${addedBlog.title}' by ${addedBlog.author} added`);
-    } catch (error) {
-      console.log(error);
-      showMessage(`an error happened: ${JSON.stringify(error.response.statusText)}`);
-    }
-
-    setState({
-      title: "",
-      author: "",
-      url: "",
-    });
-
-    const blogs = await getAll();
-    setBlogs(blogs);
-  };
-
-  const handleChange = (evt) => {
-    const value = evt.target.value;
-    setState({
-      ...state,
-      [evt.target.name]: value,
-    });
-  };
-
   const addBlogForm = () => (
-    <form onSubmit={addBlog}>
-      <div>
-        title
-        <input
-          type="text"
-          value={state.title}
-          name="title"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        author
-        <input
-          type="text"
-          value={state.author}
-          name="author"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        url
-        <input
-          type="text"
-          value={state.url}
-          name="url"
-          onChange={handleChange}
-        />
-      </div>
-      <button type="submit">create</button>
-    </form>
+    <Togglable buttonLabel="new blog" ref={blogFormRef}>
+      <BlogForm createBlog={addBlog} />
+    </Togglable>
   );
 
   return (
---
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+const BlogForm = ({ createBlog }) => {
+  const [state, setState] = useState({
+    title: "",
+    author: "",
+    url: "",
+  });
+
+  const addBlog = (event) => {
+    event.preventDefault();
+    createBlog(state);
+
+    setState({
+        title: "",
+        author: "",
+        url: "",
+      });
+  };
+
+  const handleChange = (evt) => {
+    const value = evt.target.value;
+    setState({
+      ...state,
+      [evt.target.name]: value,
+    });
+  };
+
+  return (
+    <form onSubmit={addBlog}>
+      <div>
+        title
+        <input
+          type="text"
+          value={state.title}
+          name="title"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        author
+        <input
+          type="text"
+          value={state.author}
+          name="author"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        url
+        <input
+          type="text"
+          value={state.url}
+          name="url"
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  );
+};
+
+export default BlogForm;
