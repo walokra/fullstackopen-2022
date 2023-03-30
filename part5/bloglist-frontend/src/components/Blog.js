bb080e181e6f91e2bd2e0eef91163cc0ff1e8962
Marko Wallin <marko.wallin@iki.fi>
2023-03-30 15:32:02 +0300

    feat 5b, 5.7, 5.8

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <div style={showWhenVisible}>
+        {blog.url}<br />
+        likes: {blog.likes}<br />
+        {blog.user ? blog.user.name : "unknown user"}
+      </div>
+      <button onClick={toggleVisibility}>view</button>
+    </div>
+  );
+};
+
+export default Blog;
