90b960c1d31f15cafc874f832a0377ca4f3ee71f
Marko Wallin <marko.wallin@iki.fi>
2023-04-08 17:29:38 +0300

    feat 5d, 5.18

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,7 +1,14 @@
 describe('Blog ', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    // create here a user to backend
+
+    const user = {
+      name: 'Richard Roe',
+      username: 'richard',
+      password: 'password'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+
     cy.visit('http://localhost:3000')
   })
 
@@ -11,6 +18,26 @@ describe('Blog ', function() {
 
   it('login form is shown', function() {
     cy.contains('login').click()
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('richard')
+      cy.get('#password').type('password')
+      cy.get('#login-button').click()
+
+      cy.contains('Richard Roe logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type('richard')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error').should('contain', 'wrong username or password')
+    })
+
   })
 
 })
